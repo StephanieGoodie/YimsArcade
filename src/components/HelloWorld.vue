6d1e5fe85ec22632add91a7cad34f5d906946f5b
Stephanie Goodie-Obi <goodieobistephanie@gmail.com>
2023-11-04 11:22:27 -0400

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,12 +2,15 @@
   <div>
     <h1>X & O Game</h1>
     <div id="game-board">
-      <div class="cell" v-for="(cell, index) in gameBoard" :key="index" @click="makeMove(index)">{{ cell }}</div>
+      <div class="cell" v-for="(cell, index) in gameBoard" :key="index" @click="makeMove(index)">
+        {{ cell }}
+      </div>
     </div>
     <div id="message">{{ message }}</div>
     <div id="score">
       <p>Player Wins: <span id="playerScore">{{ playerScore }}</span></p>
       <p>Computer Wins: <span id="computerScore">{{ computerScore }}</span></p>
+      <!-- Add a button for starting a new round -->
       <button id="newRoundButton" @click="startNewRound">New Round</button>
     </div>
   </div>
@@ -45,7 +48,7 @@ export default {
           this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
           this.message = `${this.currentPlayer}'s Turn`;
           if (this.currentPlayer === 'O') {
-            setTimeout(() => this.computerMove(), 500);
+            setTimeout(this.computerMove, 500); // Delay for the computer's move
           }
         }
       }
@@ -56,6 +59,7 @@ export default {
         do {
           index = Math.floor(Math.random() * 9);
         } while (this.gameBoard[index] !== '');
+
         this.makeMove(index);
       }
     },
@@ -63,11 +67,16 @@ export default {
       const winPatterns = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
-        [0, 4, 8], [2, 4, 6]
+        [0, 4, 8], [2, 4, 6],
       ];
+
       for (const pattern of winPatterns) {
         const [a, b, c] = pattern;
-        if (this.gameBoard[a] && this.gameBoard[a] === this.gameBoard[b] && this.gameBoard[a] === this.gameBoard[c]) {
+        if (
+          this.gameBoard[a] &&
+          this.gameBoard[a] === this.gameBoard[b] &&
+          this.gameBoard[a] === this.gameBoard[c]
+        ) {
           return true;
         }
       }
@@ -84,5 +93,50 @@ export default {
 </script>
 
 <style scoped>
-/* Your CSS styles here */
+body {
+  font-family: Arial, sans-serif;
+  text-align: center;
+}
+
+h1 {
+  color: #333;
+}
+
+#game-board {
+  display: grid;
+  grid-template-columns: repeat(3, 100px);
+  gap: 10px;
+  margin: 20px auto;
+  max-width: 330px;
+}
+
+.cell {
+  width: 100px;
+  height: 100px;
+  font-size: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #eee;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.cell:hover {
+  background-color: #ccc;
+}
+
+#message {
+  font-size: 1.2rem;
+  margin-top: 10px;
+}
+
+#score {
+  font-size: 1.2rem;
+  margin-top: 20px;
+}
+
+#score p {
+  margin: 5px;
+}
 </style>
