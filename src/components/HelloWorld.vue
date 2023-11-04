80d6b5726135c81d3755b5ed76218267313ae158
Stephanie Goodie-Obi <goodieobistephanie@gmail.com>
2023-11-04 11:04:48 -0400

    javascript addon

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -31,6 +31,84 @@
 </template>
 
 <script>
+const cells = document.querySelectorAll('.cell');
+const message = document.getElementById('message');
+const playerScoreDisplay = document.getElementById('playerScore');
+const computerScoreDisplay = document.getElementById('computerScore');
+
+let currentPlayer = 'X';
+let playerScore = 0;
+let computerScore = 0;
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+let gameOver = false;
+
+function makeMove(cell) {
+    const index = Array.from(cells).indexOf(cell);
+
+    if (gameBoard[index] === '' && !gameOver) {
+        gameBoard[index] = currentPlayer;
+        cell.textContent = currentPlayer;
+        cell.style.pointerEvents = 'none';
+
+        if (checkWin()) {
+            message.textContent = `${currentPlayer} wins!`;
+            if (currentPlayer === 'X') {
+                playerScore++;
+                playerScoreDisplay.textContent = playerScore;
+            } else {
+                computerScore++;
+                computerScoreDisplay.textContent = computerScore;
+            }
+            gameOver = true;
+        } else if (gameBoard.indexOf('') === -1) {
+            message.textContent = "It's a draw!";
+            gameOver = true;
+        } else {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            message.textContent = `${currentPlayer}'s Turn`;
+            if (currentPlayer === 'O') {
+                setTimeout(computerMove, 500); // Delay for computer's move
+            }
+        }
+    }
+}
+
+function computerMove() {
+    if (!gameOver) {
+        let index;
+        do {
+            index = Math.floor(Math.random() * 9);
+        } while (gameBoard[index] !== '');
+
+        makeMove(cells[index]);
+    }
+}
+
+function checkWin() {
+    const winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+
+    for (const pattern of winPatterns) {
+        const [a, b, c] = pattern;
+        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+            cells[a].style.backgroundColor = 'lightgreen';
+            cells[b].style.backgroundColor = 'lightgreen';
+            cells[c].style.backgroundColor = 'lightgreen';
+            return true;
+        }
+    }
+    return false;
+}
+
+for (const cell of cells) {
+    cell.addEventListener('click', () => makeMove(cell));
+}
+
+message.textContent = `${currentPlayer}'s Turn`;
+
 export default {
   name: 'HelloWorld',
   props: {
